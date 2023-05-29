d98042a81ae3ab9015f902556048156e3a3452cf
Aditya Veer Parmar <nakulparmar15@gmail.com>
2023-05-29 16:27:38 +0530

    retrieval qa implementation

--- backend/app/api_v1/routes/search.py
+++ backend/app/api_v1/routes/search.py
@@ -7,38 +7,57 @@ from langchain.embeddings import OpenAIEmbeddings
 from api_v1.settings import settings
 from nomic import AtlasProject
 import numpy as np
+from langchain.vectorstores import AtlasDB
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.chat_models import ChatOpenAI
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-
 class SemanticSearchResponse(BaseModel):
-    results: List[Dict] = Field(..., description="Semantic search results")
+    result: str
+
 
 router = APIRouter(prefix="/search", tags=["Search Endpoints"])
+
 
 async def load_atlas_project():
     global atlas_project
     atlas_project = AtlasProject(name=settings.atlas_project_name)
 
+
 @router.get("/", response_model=SemanticSearchResponse)
 async def semantic_search(query: str, num_results: int = 10):
-    '''
+    """
     Returns semantic search results
-    '''
-    global atlas_project
+    """
+    # global atlas_project
 
-    atlas_semantic_search = atlas_project.maps[0]
+    # atlas_semantic_search = atlas_project.maps[0]
 
-    #get the emebding for the query with open ai
-    openai = OpenAIEmbeddings(openai_api_key=settings.openai_api_key, model='text-embedding-ada-002')
-    query = np.array(openai.embed_documents(texts=[query]))
+    # # get the emebding for the query with open ai
+    openai = OpenAIEmbeddings(
+        openai_api_key=settings.openai_api_key, model="text-embedding-ada-002"
+    )
+    # query = np.array(openai.embed_documents(texts=[query]))
 
-    # perform semantic search
-    neighbors, distances = atlas_semantic_search.vector_search(queries=query, k=num_results)
+    # # perform semantic search
+    # neighbors, distances = atlas_semantic_search.vector_search(
+    #     queries=query, k=num_results
+    # )
 
-    results = atlas_project.get_data(ids=neighbors[0])
+    # results = atlas_project.get_data(ids=neighbors[0])
 
-    return SemanticSearchResponse(results=results)
+    # return SemanticSearchResponse(results=results)
+    vectorstore = AtlasDB(settings.atlas_project_name, openai, settings.nomic_api_key)
+    chain = RetrievalQAWithSourcesChain.from_chain_type(
+        ChatOpenAI(model_name="gpt-3.5-turbo"),
+        chain_type="map_reduce",
+        retriever=vectorstore.as_retriever(),
+    )
 
+    answer = chain({"question": query}, return_only_outputs=True)
+    print(answer)
+    return SemanticSearchResponse(result=answer)
